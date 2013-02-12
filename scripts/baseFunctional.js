40cce3ba59c6d4d41ad11f82b8fb72b44f98b1d8
Olga Trofimova <steshic@mail.ru>
2013-02-13 00:45:37 +0300

    Add opacity.

--- scripts/baseFunctional.js
+++ scripts/baseFunctional.js
@@ -7,125 +7,120 @@
  */
 // Home task #5.
 $(document).ready(function(){
-$("#calc_result").val(0);
-var digitArr = ['0','1','2','3','4','5','6','7','8','9'];
-var operationArr = ['/','-','+','*','='];
-var operationMap = {
-    '/':'divide',
-    '-':'minus',
-    '+':'plus',
-    '*':'multiply'
-};
-var _x1 = null;
-var _x2 = null;
-var _operation = null;
-var $current = $("#calc_result");
-var $spinner = $("#spinner");
-var $calculator = $(".calculator");
-var $history = $("#history");
+    var digitArr = ['0','1','2','3','4','5','6','7','8','9'];
+    var operationArr = ['/','-','+','*','='];
+    var operationMap = {
+        '/':'divide',
+        '-':'minus',
+        '+':'plus',
+        '*':'multiply'
+    };
+    var _x1 = null;
+    var _x2 = null;
+    var _operation = null;
+    var $current = $("#calc_result");
+    var $calculator = $(".calculator");
+    var $history = $("#history");
+    $current.val(0);
 
-$('input').bind('click', function(){
-    var btnVal = $(this).val();
-    if (_.contains(digitArr, btnVal)){
-        handleEnteredDigit(btnVal);
-    }else if(_.contains(operationArr, btnVal)){
-        handleEnteredOperation(btnVal);
-    }else{
-        switch(btnVal){
-            case '+/-':
-                signSwitch();
-                break;
-            default:
-                reset();
-                break;
+    $('input').bind('click', function(){
+        var btnVal = $(this).val();
+        if (_.contains(digitArr, btnVal)){
+            handleEnteredDigit(btnVal);
+        }else if(_.contains(operationArr, btnVal)){
+            handleEnteredOperation(btnVal);
+        }else{
+            switch(btnVal){
+                case '+/-':
+                    signSwitch();
+                    break;
+                default:
+                    reset();
+                    break;
+            }
         }
-    }
-});
+    });
 
-function handleEnteredDigit (digit)
-{
-    if($current.val().length == 8) return;
-    if(_.isNull(_operation)){
-        if(_.isNull(_x1)){
+    function handleEnteredDigit (digit)
+    {
+        if($current.val().length == 8) return;
+        if(_.isNull(_operation)){
             if($current.val() !== '0'){
                 _x1 = $current.val() +digit;
             }else{
                 _x1 = digit;
             }
+            $current.val(_x1);
         }else{
-            _x1 += digit;
+            if (_.isNull(_x1)){
+                _x1 = $current.val();
+            }
+            if(_.isNull(_x2)){
+                _x2 = digit;
+            }else{
+                _x2 += digit;
+            }
+            $current.val(_x2);
         }
-        $current.val(_x1);
-    }else{
-        if (_.isNull(_x1)){
-            _x1 = $current.val();
-        }
-        if(_.isNull(_x2)){
-            _x2 = digit;
-        }else{
-            _x2 += digit;
-        }
-        $current.val(_x2);
     }
-}
 
-function handleEnteredOperation (oper)
-{
-    if(oper != '='){
-        if (!_.isNull(_operation) && !_.isNull(_x1) && !_.isNull(_x2)){
+    function handleEnteredOperation (oper)
+    {
+        if(oper != '='){
+            if (!_.isNull(_operation) && !_.isNull(_x1) && !_.isNull(_x2)){
+                calculation();
+                _x2 = null;
+            }
+            _operation = oper;
+        }else if(!_.isNull(_x1) && !_.isNull(_x2) && !_.isNull(_operation)){
             calculation();
+            _x1 = null;
             _x2 = null;
+            _operation = null;
         }
-        _operation = oper;
-    }else if(!_.isNull(_x1) && !_.isNull(_x2) && !_.isNull(_operation)){
-        calculation();
+    }
+
+    function reset ()
+    {
+        $current.val(0);
         _x1 = null;
         _x2 = null;
         _operation = null;
     }
-}
 
-function reset ()
-{
-    $current.val(0);
-    _x1 = null;
-    _x2 = null;
-    _operation = null;
-}
-
-function signSwitch ()
-{
-    if(!_.isNull(_x1) && _.isNull(_operation) && _x1 !== 0){
-        _x1 *= -1;
-        $current.val(_x1);
-    }else if(!_.isNull(_operation) && !_.isNull(_x2) && _x2 !== 0){
-        _x2 *= -1;
-        $current.val(_x2);
-    }else if(_.isNull(_x1) && $current.val() !== 0){
-        _x1 = -1*$current.val();
-        $current.val(_x1);
-    }
-}
-
-function calculation(){
-    var transaction = '';
-    $.ajax({
-        dataType: "jsonp",
-        url: "http://train.eu01.aws.af.cm/calc.php",
-        data: {x1: _x1, x2: _x2, operation:  operationMap[_operation]},
-        success: function(data){
-            $current.val(data.result);
-            $spinner.hide();
-            $calculator.show();
-            $history.append('<p>'+transaction+data.result+'</p>')
-                .show();
-        },
-        beforeSend: function() {
-            transaction = _x1+_operation+_x2+'=';
-            $calculator.hide();
-            $history.hide();
-            $spinner.show();
+    function signSwitch ()
+    {
+        if(!_.isNull(_x1) && _.isNull(_operation) && _x1 !== 0){
+            _x1 *= -1;
+            $current.val(_x1);
+        }else if(!_.isNull(_operation) && !_.isNull(_x2) && _x2 !== 0){
+            _x2 *= -1;
+            $current.val(_x2);
+        }else if(_.isNull(_x1) && $current.val() !== 0){
+            _x1 = -1*$current.val();
+            $current.val(_x1);
         }
-    });
-}
-})
+    }
+
+    function calculation(){
+        var transaction = '';
+        $.ajax({
+            dataType: "jsonp",
+            url: "http://train.eu01.aws.af.cm/calc.php",
+            data: {x1: _x1, x2: _x2, operation:  operationMap[_operation]},
+            success: function(data){
+                $current.val(data.result);
+                $history.append('<p>'+transaction+data.result+'</p>')
+                    .show();
+                _.each($('.calc_btn'), function(btn){btn.disabled = false;});
+                $calculator.removeClass('opacity50');
+            },
+            beforeSend: function() {
+                transaction = _x1+_operation+_x2+'=';
+                _.each($('.calc_btn'), function(btn){btn.disabled = true;});
+                $calculator.addClass('opacity50');
+            }
+        });
+    }
+});
+
