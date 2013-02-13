31921ae0b0fc8a506e565d1b7f29d6eae247c115
Olga Trofimova <steshic@mail.ru>
2013-02-13 23:29:10 +0300

    Used requireJS.

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,7 @@ var current;
 var calculator;
 var history;
 
-requirejs.config={
-    paths:{
-        'jquery': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui'
-    },
-    shim: {
-        'jquery': {
-            exports: 'jQuery'
-        }
-    }
-};
-requirejs(['jquery','handlers/handleEnteredDigit', 'handlers/handleEnteredOperation', 'handlers/reset','handlers/signSwitch','handlers/calculation'],
+requirejs(['lib/jquery-1.9.1','handlers/handleEnteredDigit', 'handlers/handleEnteredOperation', 'handlers/reset','handlers/signSwitch','handlers/calculation'],
 function($, digitHandler, operationHandler, res, signSw, calc){
     digitArr = ['0','1','2','3','4','5','6','7','8','9'];
     operationArr = ['/','-','+','*','='];
@@ -31,13 +21,13 @@ function($, digitHandler, operationHandler, res, signSw, calc){
     x1 = null;
     x2 = null;
     operation = null;
-    current = $("#calc_result");
-    calculator = $(".calculator");
-    history = $("#history");
+    current = jQuery("#calc_result");
+    calculator = jQuery(".calculator");
+    history = jQuery("#history");
     current.val(0);
 
-     $('input').bind('click', function(){
-        var btnVal = $(this).val();
+     jQuery('input').bind('click', function(){
+        var btnVal = jQuery(this).val();
         if (_.contains(digitArr, btnVal)){
             handleEnteredDigit(btnVal);
         }else if(_.contains(operationArr, btnVal)){
