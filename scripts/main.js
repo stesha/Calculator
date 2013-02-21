47a299eef3311708e1c87bbbb017408ee974ee98
steshicoa <steshic@mail.ru>
2013-02-21 11:36:16 +0300

    Add bust for requireJS.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ requirejs.config({
     paths:{
         'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
         'underscore': 'http://underscorejs.org/underscore'
-    }
+    },
+    urlArgs : "bust="+new Date().getTime()
 })
 requirejs(['jquery','underscore','baseFunctional'], function($, _, bF){})
